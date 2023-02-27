a68b3f65746b6f222761e51ab5a287fc23f810e7
Prostrab <prostrab@gmail.com>
2023-02-28 00:53:16 +0300

    Задание 3

--- DomashRabSem3/Zadanie2/Program.cs
+++ DomashRabSem3/Zadanie2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+double AB (int X1, int Y1, int Z1, int X2 , int Y2, int Z2)
+
+{
+int A = Convert.ToInt32(Math.Pow(X2-X1, 2));
+int B = Convert.ToInt32(Math.Pow(Y2-Y1, 2));
+int C = Convert.ToInt32(Math.Pow(Z2-Z1, 2));
+double D = Convert.ToDouble(Math.Sqrt(A+B+C));
+
+return Math.Round(D,2);
+}
+
+
+
+Console.WriteLine($"Расстояние между точками {AB (7,-5,0,1,-1,9)}");
+
+
+
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
--- DomashRabSem3/Zadanie3/Program.cs
+++ DomashRabSem3/Zadanie3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+void Kub (int value)
+{
+    for (int i = 1; i <= value; i++)
+
+        { 
+            Console.Write($"{Math.Pow(i,3)}");
+            Console.Write(i==value? "." : ",");
+        
+
+        }
+Console.WriteLine(" ");
+
+
+}
+
+Kub (6);
